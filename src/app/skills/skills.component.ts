053e5e9bf01a3990106f818dedd391803afb3dde
jchrabie <joel.chrabie@gmail.com>
2018-06-02 11:46:41 +0200

    commit

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -5,23 +5,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
-  chartdata: boolean = false;
-
-  // Chart
-  view: number[] = [300, 500];
-  countryData = [
+  private charts = [
     {
-      'name': 'Javascript',
-      'value': 60
+      title: 'Technologie',
+      countryData: [
+        {
+          'name': 'Javascript',
+          'value': 60
+        },
+        {
+          'name': 'TypeScript',
+          'value': 40
+        }
+      ],
+      colorScheme: {
+        domain: ['#00838F', '#00BCD4']
+      },
+      label: 'toto'
     },
     {
-      'name': 'TypeScript',
-      'value': 40
+      title: 'Framework',
+      countryData: [
+        {
+          'name': 'AngularJS',
+          'value': 40
+        },
+        {
+          'name': 'Angular',
+          'value': 40
+        },
+        {
+          'name': 'Ionic',
+          'value': 20
+        }
+      ],
+      colorScheme: {
+        domain: ['#00838F', '#00BCD4', '#80DEEA']
+      }
+    },
+    {
+      countryData: [
+        {
+          'name': 'Javascript',
+          'value': 60
+        },
+        {
+          'name': 'TypeScript',
+          'value': 40
+        }
+      ],
+      colorScheme: {
+        domain: ['#00bcd4']
+      }
+    },
+    {
+      countryData: [
+        {
+          'name': 'Javascript',
+          'value': 60
+        },
+        {
+          'name': 'TypeScript',
+          'value': 40
+        }
+      ],
+      colorScheme: {
+        domain: ['#00bcd4']
+      }
     }
   ];
-  colorScheme = {
-    domain: ['#00bcd4']
-  };
 
   constructor() {
 
